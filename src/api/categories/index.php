cf6bb8f498695c45e700e300ab7c4cd624147f15
Alessandro Mazzoli <alessandro@pop-os.localdomain>
2021-12-10 17:50:18 +0100

    fix new API system

--- src/api/categories/index.php
+++ src/api/categories/index.php
@@ -2,42 +2,9 @@
 
 require_once "../api.php";
 require_once "../../db/categories/category.php";
+require_once "../../db/entity.php";
 
-class CategoriesAPI extends API {
-
-    protected function onGet(){
-        if (isset($_GET['id'])) {
-            echo json_encode(Entity::find(Category::class, $_GET['id']));
-        } else {
-            echo json_encode(Entity::all(Category::class));
-        }
-    }
-
-    protected function onPost(){
-        $name = $_POST['name'];
-        $description = $_POST['description'];
-        $res = Entity::create(Category::class, null, $name, $description);
-        echo $res;
-    }
-
-    protected function onPatch(){
-        $_PATCH = $this->getPatchData();
-        $id = $_GET['id'];
-        $name = $_PATCH['name'];
-        $description = $_PATCH['description'];
-        Entity::update(Category::class, $id, $name, $description);
-    }
-
-    protected function onDelete(){
-        if (isset($_GET['id'])) {
-            $res = Entity::delete(Category::class, $_GET['id']);
-            http_response_code($res ? 200 : 400);
-            echo json_encode($res);
-        }
-    }
-
-}
-
-API::run(new CategoriesAPI());
+$api = new API(Category::class);
+$api->handle();
 
 ?>
--- src/api/tags/index.php
+++ src/api/tags/index.php
@@ -2,41 +2,8 @@
 
 require_once "../api.php";
 require_once "../../db/tags/tag.php";
-require_once "../../db/entity.php";
-class TagsAPI extends API {
-
-    protected function onGet(){
-        if (isset($_GET['id'])) {
-            echo json_encode(Entity::find(Tag::class, $_GET['id']));
-        } else {
-            echo json_encode(Entity::all(Tag::class));
-        }
-    }
-
-    protected function onPost(){
-        $name = $_POST['name'];
-        $description = $_POST['description'];
-        $res = Entity::create(Tag::class, null, $name, $description);
-        echo $res;
-    }
-
-    protected function onPatch(){
-        $_PATCH = $this->getPatchData();
-        $id = $_GET['id'];
-        $name = $_PATCH['name'];
-        $description = $_PATCH['description'];
-        Entity::update(Tag::class, [$id, ], $name, $description);
-    }
-
-    protected function onDelete(){
-        if (isset($_GET['id'])) {
-            $res = Entity::delete(Tag::class, $_GET['id']);
-            http_response_code($res ? 200 : 400);
-            echo json_encode($res);
-        }
-    }
-}
 
 $api = new API(Tag::class);
 $api->handle();
+
 ?>
