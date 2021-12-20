fa9e3591a36493a7c01f549fb4ce9f00105a9682
manuandru <manuandru987@gmail.com>
2021-12-21 00:10:27 +0100

    implement index draft

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,6 @@
-$("body").load("home.html")
+// Code here to be executed before all.
+
+// if user is logged in, load main page
+// PageManager.switchPage(Page.MAIN);
+// else, load site home page
+PageManager.switchPage(Page.HOME);
