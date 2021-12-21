d2dfba41908f4dde0b5e1e244942af70475483e5
manuandru <manuandru987@gmail.com>
2021-12-21 15:37:20 +0100

    adjust link between pages

--- src/pages/home/home.js
+++
@@ -1,6 +0,0 @@
-
-$("#accedi").on("click", function(e) {
-    e.preventDefault();
-    window.history.pushState(Page.LOGIN, null, window.location);
-    PageManager.switchPage(Page.LOGIN);
-});
--- src/pages/login/login.js
+++
@@ -1,6 +0,0 @@
-$("html").fadeIn();
-$("#registrati").on("click", function(e) {
-    e.preventDefault();
-    window.history.pushState(Page.REGISTER, null, window.location);
-    PageManager.switchPage(Page.REGISTER);
-});
