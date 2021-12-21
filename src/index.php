f8c04a71cae870de5417b88308a1641b692418f6
manuandru <manuandru987@gmail.com>
2021-12-21 16:38:00 +0100

    now if no page is given will redirect to home

--- src/index.php
+++ src/index.php
@@ -7,14 +7,17 @@ class Pages {
     ];
 
     static function get($page) {
-        $page = strtolower($page);     
+        $page = strtolower($page);
+        if (!in_array($page, self::pages)) {
+            $page = 'error';
+        }
         require __DIR__ . "/pages/base.php";
     }
 
 }
 
 if (!isset($_GET["page"])){
-    Pages::get("error");
+    Pages::get("home");
 } else {
     Pages::get($_GET["page"]);
 }
