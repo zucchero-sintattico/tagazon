3d8682cce45d5c8e7232aef8701431313f2d0529
Alessandro Mazzoli <alemazzoli97@gmail.com>
2021-12-22 10:43:29 +0100

    multithread scarping

--- database/create/scraping.py
+++ database/create/scraping.py
@@ -1,52 +1,73 @@
 from bs4 import BeautifulSoup # BeautifulSoup is in bs4 package 
-import requests
-import os
+import json, time, os, requests
 from os import listdir
 from os.path import isfile, join
-import json
+from multiprocessing import Process
 
-mypath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'categories')
-files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
-tags = []
-for file in files:
-    with open(os.path.join(mypath, file), 'r') as f:
-        ts = [t.replace('\n', '') for t in f.readlines()]
-        for t in ts:
-            if not t in tags:
-                tags.append(t)
+THREADS = 8
+OUTPUT = dict()
 
-output = []
-for tag in tags:
-    
-    res = dict()
-    res['name'] = tag
-    
-    print(f"Tag: {tag} [{tags.index(tag) + 1}/{len(tags)}]")
-    
-    
+def getTags():
+    mypath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'categories')
+    files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+    tags = []
+    for file in files:
+        with open(os.path.join(mypath, file), 'r') as f:
+            ts = [t.replace('\n', '') for t in f.readlines()]
+            for t in ts:
+                if not t in tags:
+                    tags.append(t)
+    return tags
+
+
+def getTagDescription(tag):
     url = f'https://html.com/tags/{tag}'
     content = requests.get(url)
     soup = BeautifulSoup(content.text, 'html.parser')
     row = soup.find_all('dd')
-    res['description'] = ''
     if len(row) > 0:
         if row[0].get_text().startswith('The'): 
             row = row[0]
         else:
             row = row[1]        
-        res['description'] = row.get_text()
-    
+        return row.get_text()
+    return None
+
+def getTagExampleAndExampleDescription(tag):
     url = f"https://www.w3schools.com/TAGS/tag_{tag}.asp"
     content = requests.get(url)
-    res['example'] = ''
     if content.status_code == 200:
         soup = BeautifulSoup(content.text, 'html.parser')
-        row = soup.find('div', {'class' : 'w3-code notranslate htmlHigh'})
-        res['example'] = row.get_text()
-    
-    output.append(res)
+        row = soup.find('div', {'class': 'w3-example'})
+        example_desc = row.find('p').get_text()
+        example = row.find('div', {'class' : 'w3-code notranslate htmlHigh'}).get_text()
+        return example, example_desc
+    return None, None
 
-# write output variable on a json file
-with open('output.json', 'w') as f:
-    json.dump(output, f, indent=4)
+def scraping(tags, index):
+    global OUTPUT
+    for tag in tags:
+        print(f'Thread {index} - {tag}', flush=True)
+        res = dict({'tag': tag})
+        res['description'] = getTagDescription(tag)
+        res['example'], res['example_desc'] = getTagExampleAndExampleDescription(tag)
+        OUTPUT[tag] = res
+        
+
+
+if __name__ == '__main__':
+    tags = getTags()
+    ps = []
+    elementPerThread = int(len(tags)/THREADS) + 1
+    splittedTags = [tags[i*elementPerThread:(i+1)*elementPerThread] for i in range(0, THREADS)]
+    for splitTags in splittedTags:
+        p = Process(target=scraping, args=(splitTags, splittedTags.index(splitTags) + 1))
+        ps.append(p)
+        p.start()
+
+    for p in ps:
+        p.join()
+
+    with open('tags.json', 'w') as f:
+        json.dump(OUTPUT, f)
