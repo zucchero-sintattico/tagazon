7900d11440c0a24b65859e52ef77a0de66441a03
manuandru <manuandru987@gmail.com>
2021-12-23 00:28:48 +0100

    add animation

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -6,51 +6,91 @@ $(() => {
         }
     );
 
-    $.ajax({
-        url: "api/objects/categories",
-        type: "GET",
-        success: loadCategory,
-        error: (err) => { console.log(err); }
-    });
-
-    $.ajax({
-        url: "api/objects/tags",
-        type: "GET",
-        success: loadTags,
-        error: (err) => { console.log(err); }
-    });
+    request("api/objects/categories", loadCategory);
+    request("api/objects/tags", loadTags);
 
 });
 
-function loadCategory(data) {
-    const categories = data["data"];
-
-    categories.forEach(category => {
-        $("#categories-list").append(`
-            <li><button>${category["name"]}</button></li>
-        `);
+/**
+ * 
+ * @param {string} object to get from server
+ * @param {function} callback to execute on data["data"]
+ */
+function request(request, callback) {
+    $.ajax({
+        url: request,
+        type: "GET",
+        success: (data) => callback(data["data"]),  /* data: {code: Integer, message: String, data: Array} */
+        error: (err) => { console.log(err); }
     });
 }
 
-function loadTags(data) {
-    const tags = data["data"];
-
-    console.log(tags);
-
-    tags.forEach(tag => {
-        $("#tags-list").append(`
-            <article>
-                <header>
-                    <a href="#"><img src="res/img/icons/mail.webp" alt="info"></a>
-                    <h3>${tag["name"]}</h3>
-                </header>
-                    <p>${encodeStr(tag["description"])}</p>
-                <footer>
-                    <p>${tag["price"]}€</p>
-                </footer>
-            </article>
-        `);
+/**
+ *  @param {Array} categories to load in the list
+ */
+function loadCategory(categories) {
+    categories.forEach(category => {
+        const li = document.createElement("li");
+        const button = document.createElement("button");
+        button.categoryId = category["id"];
+        button.innerText = category["name"];
+        button.addEventListener("click", handleChangeCategory);
+        li.appendChild(button);
+        $("#categories-list").append(li);
     });
+}
+
+/**
+ * Event handler for the category button
+ */
+function handleChangeCategory() {
+    if ($(this).hasClass("selected")) {
+        $(this).removeClass("selected");
+
+        request("api/objects/tags", loadTags);
+    } else {
+        $("#categories-list > li > button").removeClass("selected");
+        $(this).addClass("selected");
+
+        request(
+            `api/objects/categories/tags/?category_id=${this.categoryId}`,
+            (tags) => tags.length > 0 ? loadTags(tags) : request("api/objects/tags", loadTags),
+        );
+    }
+}
+
+/**
+ * 
+ * @param {Array} tags to load in the list of tags
+ */
+function loadTags(tags) {
+
+    $("#tags-list").fadeOut(500, () => {
+        removeTags();
+        tags.forEach(tag => {
+            $("#tags-list").append(`
+                <article>
+                    <header>
+                        <a href="#"><img src="res/img/icons/mail.webp" alt="info"></a>
+                        <h3>${tag["name"]}</h3>
+                    </header>
+                        <p>${encodeStr(tag["description"])}</p>
+                    <footer>
+                        <p>${tag["price"]}€</p>
+                    </footer>
+                </article>
+            `);
+        });
+        $("#tags-list").fadeIn(250);
+    })
+
+}
+
+/**
+ * 
+ */
+function removeTags() {
+    document.getElementById("tags-list").innerHTML = "";
 }
 
 function encodeStr(str) {
