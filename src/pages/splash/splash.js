1ed29be4addb8cff33516734d53d3bbf7a55cb72
Alex Testa <alex.testa@studio.unibo.it>
2021-12-29 13:09:43 +0100

    Replace splash screen page

--- src/pages/splash/splash.js
+++ src/pages/splash/splash.js
@@ -1,3 +1,4 @@
+/*
 $(() => {
     UserManager.start(
         ifLogged = () => {
@@ -6,3 +7,36 @@ $(() => {
         ifNotLogged = () => {}
     );
 })
+*/
+$(document).ready(() => {
+    /* Span Click */
+    $("span").click(function (e) {
+        e.preventDefault();
+        removeAllActiveItem();
+        activeItem(this);
+        showCorrectPage(this);
+    });
+
+
+});
+
+
+function activeItem(item){
+    $(item).addClass('item-active');
+}
+
+function removeAllActiveItem(){
+    $("body > main > footer > div span").each(function (index){
+        $(this).removeClass('item-active');
+    });
+}
+
+function showCorrectPage(item){
+    $("body > main > footer > div span").each(function (index){
+        if($(this).is(item)){
+            $("#" + index).removeClass('page-hide');
+        }else{
+            $("#" + index).addClass('page-hide');
+        }
+    });
+}
