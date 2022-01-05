8736a6eb0e3f30aa8f918be53d01674edf006f3d
Alessandro Mazzoli <alemazzoli97@gmail.com>
2022-01-05 21:05:12 +0100

    added automatic slide

--- src/pages/splash/splash.js
+++ src/pages/splash/splash.js
@@ -1,41 +1,42 @@
-/*
-$(() => {
-    UserManager.start(
-        ifLogged = () => {
-            window.location.href = './?page=home';
-        },
-        ifNotLogged = () => {}
-    );
-})
-*/
 $(document).ready(() => {
     /* Span Click */
-    $("span").click(function (e) {
+    $("span").click(function(e) {
         e.preventDefault();
         removeAllActiveItem();
         activeItem(this);
         showCorrectPage(this);
     });
 
+    setInterval(nextSlide, 5000);
 
 });
 
+function nextSlide() {
+    const actual = $("span.item-active");
+    let next = actual.next();
+    if (next.length == 0) {
+        next = $("span").first();
+    }
+    actual.removeClass('item-active');
+    next.addClass('item-active');
+    showCorrectPage(next);
+}
 
-function activeItem(item){
+function activeItem(item) {
     $(item).addClass('item-active');
 }
 
-function removeAllActiveItem(){
-    $("body > main > footer > div span").each(function (index){
+function removeAllActiveItem() {
+    $("body > main > footer > div span").each(function(index) {
         $(this).removeClass('item-active');
     });
 }
 
-function showCorrectPage(item){
-    $("body > main > footer > div span").each(function (index){
-        if($(this).is(item)){
+function showCorrectPage(item) {
+    $("body > main > footer > div span").each(function(index) {
+        if ($(this).is(item)) {
             $("#" + index).removeClass('page-hide');
-        }else{
+        } else {
             $("#" + index).addClass('page-hide');
         }
     });
